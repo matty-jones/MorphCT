4ab1ffc4a9d11074cc32a534c225f483175dceac
Matty Jones <mattyjones@boisestate.edu>
2016-06-24 18:02:45 -0600

    Turned off debugging

--- code/hoppingKMC.py
+++ code/hoppingKMC.py
@@ -20,12 +20,12 @@ kB = 1.3806488E-23 # m^{2} kg s^{-2} K^{-1}
 hbar = 1.05457173E-34 # m^{2} kg s^{-1}
 temperature = 290 # K
 defaultSimTime = 5e-6
-limitByHops = True
+limitByHops = False
 hopLimit = 5000
 numberOfCarriersToSimulate = 100
 
 # Flag for outputting all hop times to a CSV (slow!)
-debug = True
+debug = False
 ###
 
 
