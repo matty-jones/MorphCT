Bug in TI calculation for the pairs due to incorrect neighbour lookup. Can fix it next week

code/executeZINDO.py
@@ -13,6 +13,7 @@ def createInputFiles(chromophoreList, AAMorphologyDict, parameterDict):
         # Include the molecule terminating units on the required atoms of the chromophore
         terminatingGroupPositions = terminateMonomers(chromophore, parameterDict, AAMorphologyDict)
         writeOrcaInp(AAMorphologyDict, chromophore.AAIDs, [chromophore.image] * len(chromophore.AAIDs), terminatingGroupPositions, [chromophore.image] * len(terminatingGroupPositions), chromophore.orcaInput)
+    print ""
     # Determine how many pairs there are first:
     numberOfPairs = 0
     for chromo in chromophoreList:
@@ -47,6 +48,7 @@ def createInputFiles(chromophoreList, AAMorphologyDict, parameterDict):
             inputName = chromophore1.orcaInput.replace('.inp', '-%04d.inp' % (chromophore2.ID)).replace('single', 'pair')
             # Write the dimer input file
             writeOrcaInp(AAMorphologyDict, AAIDs, images, terminatingGroupPositions1 + terminatingGroupPositions2, terminatingGroupImages1 + terminatingGroupImages2, inputName)
+    print ""
 
 
 def removeAdjacentTerminators(group1, group2):
@@ -126,7 +128,7 @@ def terminateMonomers(chromophore, parameterDict, AAMorphologyDict):
     return newHydrogenPositions
 
 
-def getORCAJobs(inputDir):
+def getORCAJobs(inputDir, procIDs):
     # First delete any previous log files as we're about to start again with the ZINDO/S calculations
     try:
         os.unlink(inputDir.replace('/inputORCA', '/*.log'))
@@ -154,23 +156,18 @@ def getORCAJobs(inputDir):
     popList.sort(reverse=True)
     for popIndex in popList:
         ORCAFilesToRun.pop(popIndex)
-    # Now split the list of remaining jobs based on the number of processors
-    try:
-        procIDs = list(np.arange(int(os.environ.get('SLURM_NPROCS'))))
-    except (AttributeError, TypeError):
-        # Was not loaded using SLURM, so use all physical processors
-        procIDs = list(np.arange(mp.cpu_count()))
     if len(ORCAFilesToRun) == 0:
-        return procIDs, []
+        return []
     # Create a jobslist for each procID
     jobsList = [ORCAFilesToRun[i:i + (int(np.ceil(len(ORCAFilesToRun) / len(procIDs)))) + 1] for i in xrange(0, len(ORCAFilesToRun), int(np.ceil(len(ORCAFilesToRun) / float(len(procIDs)))))]
-    return procIDs, jobsList
+    return jobsList
 
 
 def execute(AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList):
     createInputFiles(chromophoreList, AAMorphologyDict, parameterDict)
     inputDir = parameterDict['outputDir'] + '/' + parameterDict['morphology'][:-4] + '/chromophores/inputORCA'
-    procIDs, jobsList = getORCAJobs(inputDir)
+    procIDs = parameterDict['procIDs']
+    jobsList = getORCAJobs(inputDir, procIDs)
     numberOfInputs = sum([len(ORCAFilesToRun) for ORCAFilesToRun in jobsList])
     print "Found", numberOfInputs, "ORCA files to run."
     if numberOfInputs > 0:
@@ -185,7 +182,7 @@ def execute(AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, c
         # Open the required processes to execute the ORCA jobs
         for CPURank in procIDs:
             print 'python ' + os.getcwd() + '/code/singleCoreRunORCA.py ' + parameterDict['outputDir'] + '/' + parameterDict['morphology'][:-4] + ' ' + str(CPURank) + ' &'
-            runningJobs.append(sp.Popen(['python', str(os.getcwd()) + '/code/singleCoreRunORCA.py', parameterDict['outputDir'] + '/' + parameterDict['morphology'][:-4] + ' ' + str(CPURank)]))
+            runningJobs.append(sp.Popen(['python', str(os.getcwd()) + '/code/singleCoreRunORCA.py', parameterDict['outputDir'] + '/' + parameterDict['morphology'][:-4], str(CPURank)]))
         # Wait for all jobs to complete
         [p.wait() for p in runningJobs]
         # Delete the job pickle

code/helperFunctions.py
@@ -1028,38 +1028,14 @@ def writeORCAInp(inputDictList, outputDir, mode):
         raw_input("Hit return to continue...")
 
 
-def getORCAJobs(inputDir):
-    singleORCAFileList = os.listdir(inputDir + '/single')
-    pairORCAFileList = os.listdir(inputDir + '/pair')
-    ORCAFilesToRun = []
-    for fileName in singleORCAFileList:
-        if fileName[-4:] == '.inp':
-            ORCAFilesToRun.append(inputDir + '/single/' + fileName)
-    for fileName in pairORCAFileList:
-        if fileName[-4:] == '.inp':
-            ORCAFilesToRun.append(inputDir + '/pair/' + fileName)
-    ORCAFilesToRun.sort()
-    # Delete any jobs that have already at least started running
-    popList = []
-    for jobNo, job in enumerate(ORCAFilesToRun):
-        try:
-            with open(job.replace('inputORCA', 'outputORCA').replace('.inp', '.out'), 'r'):
-                popList.append(jobNo)
-        except IOError:
-            pass
-    popList.sort(reverse=True)
-    for popIndex in popList:
-        ORCAFilesToRun.pop(popIndex)
-    # Now split the list of remaining jobs based on the number of processors
+def getCPUCores():
+    # Determine the number of available processors, either by querying the SLURM_NPROCS environment variable, or by using multiprocessing to count the number of visible CPUs.
     try:
         procIDs = list(np.arange(int(os.environ.get('SLURM_NPROCS'))))
     except (AttributeError, TypeError):
         # Was not loaded using SLURM, so use all physical processors
         procIDs = list(np.arange(mp.cpu_count()))
-    if len(ORCAFilesToRun) == 0:
-        return procIDs, []
-    jobsList = [ORCAFilesToRun[i:i + (int(np.ceil(len(ORCAFilesToRun) / len(procIDs)))) + 1] for i in xrange(0, len(ORCAFilesToRun), int(np.ceil(len(ORCAFilesToRun) / float(len(procIDs)))))]
-    return procIDs, jobsList
+    return procIDs
 
 
 def writeToFile(logFile, stringList, mode='logFile'):

code/hoppingKMC.py
@@ -1,8 +1,6 @@
 import os
 import sys
-import csv
 import numpy as np
-import cPickle as pickle
 import random as R
 import matplotlib
 matplotlib.use('Agg')
@@ -18,15 +16,121 @@ plottingSubroutines = False
 elementaryCharge = 1.60217657E-19 # C
 kB = 1.3806488E-23 # m^{2} kg s^{-2} K^{-1}
 hbar = 1.05457173E-34 # m^{2} kg s^{-1}
-temperature = 290 # K
-defaultSimTime = 5e-6
-limitByHops = False
-hopLimit = 5000
-numberOfCarriersToSimulate = 10000
 
 # Flag for outputting all hop times to a CSV (slow!)
 debug = False
 ###
+
+
+class carrier:
+    def __init__(self, chromophoreList, parameterDict, chromoID, lifetime):
+        if parameterDict['recordCarrierHistory'] is True:
+            self.carrierHistoryMatrix = np.zeros((len(chromophoreList), len(chromophoreList)), dtype = int)
+        else:
+            self.carrierHistoryMatrix = None
+        self.image = [0, 0, 0]
+        self.currentChromophore = chromophoreList[chromoID]
+        self.lifetime = lifetime
+        self.currentTime = 0.0
+
+
+def execute(AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList):
+    # Determine the maximum simulation times based on the parameter dictionary
+    simulationTimes = np.logspace(np.log10(parameterDict['minimumSimulationTime']), np.log10(parameterDict['maximumSimulationTime']), len(parameterDict['procIDs']))
+    carrierList = []
+    # For each specifed carrier
+    for carrierNo in range(len(numberOfCarriersPerSimulationTime)):
+        # For each specified simulation time (do the loops this way round to even out the job lists when we split all the KMC jobs over the CPUs)
+        for lifetime in simulationTimes:
+            # Find an initial position (for now, just pick a chromophore at random)
+            startChromoID = R.randint(0, len(chromophoreList) - 1)
+            carrierList.append(carrier(chromophoreList, parameterDict, chromoID, lifetime))
+    # The carrierList is now like the ORCAJobsList, so split it over each procID
+
+    # Then run a bunch of singleCoreRunKMC jobs
+
+    # In that, probably make a csv file with the important data, to save us having to dump to the pickle every 5 minutes
+
+
+    return AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList
+
+
+if __name__ == "__main__":
+    try:
+        pickleFile = sys.argv[1]
+    except:
+        print "Please specify the pickle file to load to continue the pipeline from this point."
+    AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList = helperFunctions.loadPickle(pickleFile)
+    execute(AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class chargeCarrier:

code/transferIntegrals.py
@@ -136,7 +136,7 @@ def revertORCAFiles(inputFile):
         fileName.writelines(originalLines)
 
 
-def rerunFails(failedChromoFiles):
+def rerunFails(failedChromoFiles, procIDs):
     print "There were", len(failedChromoFiles.keys()), "failed jobs."
     # Firstly, modify the input files to see if numerical tweaks make ORCA happier
     for failedFile, failedData in failedChromoFiles.iteritems():
@@ -149,11 +149,6 @@ def rerunFails(failedChromoFiles):
         return failedChromoFiles
     # Otherwise, rerun those failed files
     # As before, split the list of reruns based on the number of processors
-    try:
-        procIDs = list(np.arange(int(os.environ.get('SLURM_NPROCS'))))
-    except (AttributeError, TypeError):
-        # Was not loaded using SLURM, so use all physical processors
-        procIDs = list(np.arange(mp.cpu_count()))
     jobsList = [failedChromoFiles.keys()[i:i+(int(np.ceil(len(failedChromoFiles)/len(procIDs))))+1] for i in xrange(0, len(failedChromoFiles), int(np.ceil(len(failedChromoFiles)/float(len(procIDs)))))]
     # Write the jobs pickle for singleCoreRunORCA to obtain
     with open(CSVDir+'/ORCAJobs.pickle', 'w+') as pickleFile:
@@ -229,7 +224,7 @@ def updateSingleChromophoreList(chromophoreList, parameterDict):
             failedSingleChromos[fileName] = [1, chromoLocation]
     # Rerun any failed ORCA jobs
     while len(failedSingleChromos) > 0:
-        failedSingleChromos = rerunFails(failedSingleChromos)
+        failedSingleChromos = rerunFails(failedSingleChromos, parameterDict['procIDs'])
         # Now check all of the files to see if we can update the chromophoreList
         for chromoName, chromoData in failedSingleChromos.iteritems():
             chromoID = chromoData[1]
@@ -255,48 +250,48 @@ def updatePairChromophoreList(chromophoreList, parameterDict):
     failedPairChromos = {}
     for chromoLocation, chromophore in enumerate(chromophoreList):
         for neighbourID in chromophore.neighbours:
-            if chromophore.ID > neighbourID:
+            if chromophore.ID > neighbourID[0]:
                 continue
-            fileName = 'pair/%04d-%04d.out' % (chromophore.ID, neighbourID)
+            fileName = 'pair/%04d-%04d.out' % (chromophore.ID, neighbourID[0])
             try:
                 dimerHOMO_1, dimerHOMO, dimerLUMO, dimerLUMO_1 = loadORCAOutput(orcaOutputDir + fileName)
                 # Calculate the deltaE between the two single chromophores
-                deltaE, species = calculateDeltaE(chromophoreList, chromophore.ID, neighbourID)
+                deltaE, species = calculateDeltaE(chromophoreList, chromophore.ID, neighbourID[0])
                 # Calculate the TI using the ESD method
                 if species == 'Donor':
                     TI = calculateTI(dimerHOMO - dimerHOMO_1, deltaE)
                 elif species == 'Acceptor':
                     TI = calculateTI(dimerLUMO - dimerLUMO_1, deltaE)
                 # Update both the current chromophore and the neighbour (for the reverse hop)
-                chromophore.neighboursDeltaE[chromophore.neighbours.index(neighbourID)] = deltaE
-                chromophoreList[neighbourID].neighboursDeltaE[chromophoreList[neighbourID].neighbours.index(chromophore.ID)] = - deltaE
-                chromophore.neighboursTI[chromophore.neighbours.index(neighbourID)] = TI
-                chromophoreList[neighbourID].neighboursTI[chromophoreList[neighbourID].neighbours.index(chromophore.ID)] = TI
+                chromophore.neighboursDeltaE[chromophore.neighbours.index(neighbourID[0])] = deltaE
+                chromophoreList[neighbourID[0]].neighboursDeltaE[chromophoreList[neighbourID[0]].neighbours.index(chromophore.ID)] = - deltaE
+                chromophore.neighboursTI[chromophore.neighbours.index(neighbourID[0])] = TI
+                chromophoreList[neighbourID[0]].neighboursTI[chromophoreList[neighbourID[0]].neighbours.index(chromophore.ID)] = TI
                 if parameterDict['removeORCAInputs'] is True:
                     os.remove(orcaOutputDir.replace('outputORCA', 'inputORCA') + fileName.replace('.inp','.*'))
                 if parameterDict['removeORCAOutputs'] is True:
                     os.remove(orcaOutputDir + fileName)
             except ORCAError:
-                failedPairChromos[fileName] = [1, chromoLocation, neighbourID]
+                failedPairChromos[fileName] = [1, chromoLocation, neighbourID[0]]
     while len(failedPairChromos) > 0:
-        failedPairChromos = rerunFails(failedPairChromos)
+        failedPairChromos = rerunFails(failedPairChromos, parameterDict['procIDs'])
         for chromoName, chromoData in failedPairChromos.iteritems():
             chromo1ID = chromoData[1]
             chromo2ID = chromoData[2]
             try:
                 dimerHOMO_1, dimerHOMO, dimerLUMO, dimerLUMO_1 = loadORCAOutput(orcaOutputDir + fileName)
                 # Calculate the deltaE between the two single chromophores
-                deltaE, species = calculateDeltaE(chromophoreList, chromophore.ID, neighbourID)
+                deltaE, species = calculateDeltaE(chromophoreList, chromophore.ID, neighbourID[0])
                 # Calculate the TI using the ESD method
                 if species == 'Donor':
                     TI = calculateTI(dimerHOMO - dimerHOMO_1, deltaE)
                 elif species == 'Acceptor':
                     TI = calculateTI(dimerLUMO - dimerLUMO_1, deltaE)
                 # Update both the current chromophore and the neighbour (for the reverse hop)
-                chromophore.neighboursDeltaE[chromophore.neighbours.index(neighbourID)] = deltaE
-                chromophoreList[neighbourID].neighboursDeltaE[chromophoreList[neighbourID].neighbours.index(chromophore.ID)] = - deltaE
-                chromophore.neighboursTI[chromophore.neighbours.index(neighbourID)] = TI
-                chromophoreList[neighbourID].neighboursTI[chromophoreList[neighbourID].neighbours.index(chromophore.ID)] = TI
+                chromophore.neighboursDeltaE[chromophore.neighbours.index(neighbourID[0])] = deltaE
+                chromophoreList[neighbourID[0]].neighboursDeltaE[chromophoreList[neighbourID[0]].neighbours.index(chromophore.ID)] = - deltaE
+                chromophore.neighboursTI[chromophore.neighbours.index(neighbourID[0])] = TI
+                chromophoreList[neighbourID[0]].neighboursTI[chromophoreList[neighbourID[0]].neighbours.index(chromophore.ID)] = TI
                 failedPairChromos.pop(chromoName)
                 if parameterDict['removeORCAInputs'] is True:
                     os.remove(orcaOutputDir.replace('outputORCA', 'inputORCA') + fileName.replace('.inp','.*'))
@@ -319,10 +314,14 @@ def scaleEnergies(chromophoreList, parameterDict):
             donorLevels.append(chromo.HOMO)
         elif (chromo.species == 'Acceptor'):
             acceptorLevels.append(chromo.LUMO)
-    avHOMO = np.average(donorLevels)
-    avLUMO = np.average(acceptorLevels)
-    deltaEHOMO = 0.0
-    deltaELUMO = 0.0
+    if len(donorLevels) > 0:
+        avHOMO = np.average(donorLevels)
+        stdHOMO = np.std(np.array(donorLevels))
+        deltaEHOMO = 0.0
+    if len(acceptorLevels) > 0:
+        avLUMO = np.average(acceptorLevels)
+        stdLUMO = np.std(np.array(acceptorLevels))
+        deltaELUMO = 0.0
     # Then add the lateral shift to to the energy levels to put the mean in line with the literature value
     # This is justified because we treat each chromophore in exactly the same way. Any deviation between the average of the calculated MOs and the literature one is therefore a systematic error arising from the short chromophore lengths and the frequency of the terminating groups in order to perform the DFT calculations.
     # By shifting the mean back to the literature value, we are accounting for this systematic error.
@@ -352,8 +351,6 @@ def scaleEnergies(chromophoreList, parameterDict):
         # No squeezing necessary, return the chromophoreList
         pass
     else:
-        stdHOMO = np.std(np.array(donorLevels))
-        stdLUMO = np.std(np.array(acceptorLevels))
         for chromo in chromophoreList:
             if (chromo.species == 'Donor') and (parameterDict['targetDoSSTDHOMO'] is not None):
                 # Determine how many sigmas away from the mean this datapoint is
@@ -371,43 +368,48 @@ def scaleEnergies(chromophoreList, parameterDict):
             chromo.HOMO += deltaE
             chromo.LUMO += deltaE
             chromo.LUMO_1 += deltaE
-    return chromoList
+    return chromophoreList
 
 
-def execute(AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList)
+def execute(AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList):
     # First, check that we need to examine the single chromophores
-    if overwriteCurrentData is False:
+    runSingles = False
+    if parameterDict['overwriteCurrentData'] is False:
         # Only perform this check if the user hasn't already specified to overwrite the data (in which case it runs anyway)
-        runSingles == False
         # Run all singles if any of the single's data is missing (i.e. the HOMO level should suffice because all energy levels are updated at the same time, so we don't need to check all of them individually)
         for chromophore in chromophoreList:
             if chromophore.HOMO is None:
                 runSingles = True
-                break
     if (runSingles is True) or (parameterDict['overwriteCurrentData'] is True):
         print "Beginning analysis of single chromophores..."
         chromophoreList = updateSingleChromophoreList(chromophoreList, parameterDict)
         # Now include any scaling to narrow the DoS or modulate the mean to match the literature HOMO/LUMO levels (which helps to negate the effect of short chromophores with additional hydrogens/terminating groups
+        print "Scaling energies..."
         chromophoreList = scaleEnergies(chromophoreList, parameterDict)
         print "Single chromophore calculations completed. Saving..."
+        pickleName = parameterDict['outputDir'] + '/' + parameterDict['morphology'][:-4] + '/code/' + parameterDict['morphology'][:-4] + '.pickle'
         helperFunctions.writePickle((AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList), pickleName)
     else:
         print "All single chromophore calculations already performed. Skipping..."
     # Then, check the pairs
-    if overwriteCurrentData is False:
-        runPairs == False:
-            for chromophore in chromophoreList:
-                # Just check the first neighbour for each chromophore
-                if chromophore.neighboursTI[0] is None:
-                    runPairs = True
-                    break
+    runPairs = False
+    if parameterDict['overwriteCurrentData'] is False:
+        for chromophore in chromophoreList:
+            # Just check the first neighbour for each chromophore
+            if chromophore.neighboursTI[0] is None:
+                runPairs = True
+                break
     if (runPairs is True) or (parameterDict['overwriteCurrentData'] is True):
         print "Beginning analysis of chromophore pairs..."
         chromophoreList = updatePairChromophoreList(chromophoreList, parameterDict)
+        for chromo in chromophoreList:
+            print chromo.neighboursTI
+        exit()
         print "Pair chromophore calculations completed. Saving..."
         helperFunctions.writePickle((AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList), pickleName)
     else:
         print "All pair chromophore calculations already performed. Skipping..."
+    return AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList
 
 
 if __name__ == "__main__":