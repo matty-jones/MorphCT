Bug fixes, memory optimisations and start of new mobility calculation analysis script.

analysis/KMCOut/plotMob.py
@@ -2,15 +2,82 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
+import cPickle as pickle
 import scipy.optimize
 import scipy.stats
+from scipy.sparse import lil_matrix
+from scipy.sparse import find as findNonZero
 
 
 elementaryCharge = 1.60217657E-19  # C
 kB = 1.3806488E-23  # m^{2} kg s^{-2} K^{-1}
 temperature = 290  # K
 
-if __name__ == "__main__":
-    directory = sys.argv[1]
 
+def combinePickleFiles(directory):
+    pickleFiles = []
+    for fileName in os.listdir(directory):
+        if 'KMCData' == fileName[:7]:
+            pickleFiles.append(directory + '/' + fileName)
+    print len(pickleFiles), "pickle files found to concatenate."
+    print "Concatenating data..."
+    carrierList = []
+    for pickleNo, pickleFileName in enumerate(pickleFiles):
+        print "\rLoading data", pickleNo + 1, "of", len(pickleFiles),
+        sys.stdout.flush()
+        with open(pickleFileName, 'r') as pickleFile:
+            carrierList += pickle.load(pickleFile)
+    print "\nAll data concatenated!"
+    print "Writing combined pickle..."
+    with open(directory + '/combinedKMCData.pickle', 'w+') as pickleFile:
+        pickle.dump(carrierList, pickleFile)
+    return carrierList
+
+
+def getData(carrierList):
+    squaredDisps = {}
+    noChromophoresVisited = {}
+    completeCarrierHistory = lil_matrix(carrierList[0].shape, dtype = int)
+    totalDataPoints = 0
+    totalDataPointsAveragedOver = 0
+    for carrier in carrierList:
+        if (carrier.currentTime > carrier.lifetime * 2) or (carrier.currentTime < carrier.lifetime / 2.0) or (carrier.noHops == 1):
+            totalDataPoints += 1
+            continue
+        carrierKey = str(carrier.lifetime)
+        if carrierKey not in squaredDisps:
+            squaredDisps[carrierKey] = [carrier.displacement ** 2]
+            noChromophoresVisited[carrierKey] = [len(findNonZero(carrier.carrierHistoryMatrix)[0])]
+        else:
+            squaredDisps[carrierKey].append(carrier.displacement ** 2)
+            noChromophoresVisited[carrierKey].append(len(findNonZero(carrier.carrierHistoryMatrix)[0]))
+        completeCarrierHistory += carrier.carrierHistoryMatrix
+        totalDataPointsAveragedOver += 1
+        totalDataPoints += 1
+    times = []
+    MSDs = []
+    for time, disps in squaredDisps.iteritems():
+        times.append(float(time))
+        MSDs.append(np.average(disps))
+
+
+
+
+if __name__ == "__main__":
+    sys.path.append('../../code')
+    directory = os.getcwd() + '/' + sys.argv[1]
+    print "Combining Pickle Files..."
+    needToCombine = True
+    for fileName in os.listdir(directory):
+        if 'combinedKMCData' in fileName:
+            combinedPickleName = fileName
+            needToCombine = False
+            break
+    if needToCombine is True:
+        print "combinedKMCData.pickle not found! Combining data..."
+        carrierList = combinePickleFiles(directory)
+    else:
+        with open(directory + '/combinedKMCData.pickle', 'r') as pickleFile:
+            carrierList = pickle.load(pickleFile)
+    print "Carrier List obtained!"
+    getData(carrierList)

code/hoppingKMC.py
@@ -43,8 +43,7 @@ class carrier:
         self.T = parameterDict['systemTemperature']
         self.lifetime = lifetime
         self.currentTime = 0.0
-        self.chromophoreList = chromophoreList
-        self.parameterDict = parameterDict
+        self.lambdaij = parameterDict['reorganisationEnergy']
         self.noHops = 0
         self.simDims = [[-AAMorphologyDict['lx'] / 2.0, AAMorphologyDict['lx'] / 2.0], [-AAMorphologyDict['ly'] / 2.0, AAMorphologyDict['ly'] / 2.0], [-AAMorphologyDict['lz'] / 2.0, AAMorphologyDict['lz'] / 2.0]]
         self.displacement = None
@@ -68,7 +67,7 @@ class carrier:
             tau = 1E99
         return tau
 
-    def calculateHop(self):
+    def calculateHop(self, chromophoreList):
         # Terminate if the next hop would be more than the termination limit
         if self.hopLimit is not None:
             if self.noHops + 1 > self.hopLimit:
@@ -76,18 +75,17 @@ class carrier:
         # Determine the hop times to all possible neighbours
         hopTimes = []
         # Obtain the reorganisation energy in J (from eV in the parameter file)
-        lambdaij = self.parameterDict['reorganisationEnergy']
         for neighbourIndex, transferIntegral in enumerate(self.currentChromophore.neighboursTI):
             deltaEij = self.currentChromophore.neighboursDeltaE[neighbourIndex]
             # All of the energies are in eV currently, so convert them to J
-            hopRate = self.calculateHopRate(lambdaij * elementaryCharge, transferIntegral * elementaryCharge, deltaEij * elementaryCharge)
+            hopRate = self.calculateHopRate(self.lambdaij * elementaryCharge, transferIntegral * elementaryCharge, deltaEij * elementaryCharge)
             hopTime = self.determineHopTime(hopRate)
             # Keep track of the chromophoreID and the corresponding tau
             hopTimes.append([self.currentChromophore.neighbours[neighbourIndex][0], hopTime])
         # Sort by ascending hop time
         hopTimes.sort(key = lambda x:x[1])
         # Take the quickest hop
-        destinationChromophore = self.chromophoreList[hopTimes[0][0]]
+        destinationChromophore = chromophoreList[hopTimes[0][0]]
         # As long as we're not limiting by the number of hops:
         if self.hopLimit is None:
             # Ensure that the next hop does not put the carrier over its lifetime
@@ -127,7 +125,7 @@ class carrier:
 
 def execute(AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList):
     # Determine the maximum simulation times based on the parameter dictionary
-    simulationTimes = np.logspace(np.log10(parameterDict['minimumSimulationTime']), np.log10(parameterDict['maximumSimulationTime']), len(parameterDict['procIDs']))
+    simulationTimes = parameterDict['simulationTimes']
     carrierList = []
     # For each specifed carrier
     for carrierNo in range(parameterDict['numberOfCarriersPerSimulationTime']):
@@ -142,7 +140,7 @@ def execute(AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, c
     procIDs = parameterDict['procIDs']
     outputDir = parameterDict['outputDir'] + '/' + parameterDict['morphology'][:-4] + '/KMC'
     jobsList = [carrierList[i:i + (int(np.ceil(len(carrierList) / len(procIDs)))) + 1] for i in xrange(0, len(carrierList), int(np.ceil(len(carrierList)/float(len(procIDs)))))]
-    # Create pickle file containing the jobs sorted by ProcID to be picked up by singleCoreRunORCA.py
+    # Create pickle file containing the jobs sorted by ProcID to be picked up by singleCoreRunKMC.py
     pickleName = outputDir + '/carrierJobs.pickle'
     print "Writing job pickle for each CPU..."
     with open(pickleName, 'w+') as pickleFile:

code/singleCoreRunKMC.py
@@ -72,6 +72,14 @@ if __name__ == '__main__':
         # helperFunctions.writeToFile(logFile, affinityJob[1].split('\n')) #stdErr for affinity set
     except OSError:
         helperFunctions.writeToFile(logFile, ["Taskset command not found, skipping setting of processor affinity..."])
+    # Now load the main morphology pickle (used as a workaround to obtain the chromophoreList without having to save it in each carrier [very memory inefficient!])
+    pickleDir = KMCDirectory.replace('/KMC', '/code')
+    for fileName in os.listdir(pickleDir):
+        if 'pickle' in fileName:
+            mainMorphologyPickleName = pickleDir + '/' + fileName
+    helperFunctions.writeToFile(logFile, ['Found main morphology pickle file at ' + mainMorphologyPickleName + '! Loading data...'])
+    AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, parameterDict, chromophoreList, carrierList = helperFunctions.loadPickle(mainMorphologyPickleName)
+    helperFunctions.writeToFile(logFile, ['Main morphology pickle loaded!'])
     # TEST THIS
     # Attempt to catch a kill signal to ensure that we save the pickle before termination
     killer = terminationSignal()
@@ -90,7 +98,7 @@ if __name__ == '__main__':
             helperFunctions.writeToFile(logFile, ['Running carrier %d for %.2E...' % (carrier.ID, carrier.lifetime)])
             terminateSimulation = False
             while terminateSimulation is False:
-                terminateSimulation = bool(carrier.calculateHop())
+                terminateSimulation = bool(carrier.calculateHop(chromophoreList))
                 if killer.killSent is True:
                     raise Terminate('Kill command sent, terminating KMC simulation...')
             # Now the carrier has finished hopping, let's calculate its vitals
@@ -116,7 +124,7 @@ if __name__ == '__main__':
             helperFunctions.writeToFile(logFile, ['Carrier hopped ' + str(carrier.noHops) + ' times into image ' + str(carrier.image) + ' for a displacement of ' + str(carrier.displacement) + ' in ' + str(elapsedTime) + ' ' + str(timeunits)])
             # Save the pickle file as a `checkpoint' either every 100 carriers or every 1%, whichever is greater
             if jobNumber in checkpointCarriers:
-                print "Completed", jobNumber, "jobs. Making checkpoint at %3d%%" % (np.round(jobNumber / float(len(jobsToRun)) * 100))
+                print "Completed", jobNumber, "jobs. Making checkpoint at %3d%%" % (np.round(jobNumber + 1 / float(len(jobsToRun)) * 100))
                 helperFunctions.writeToFile(logFile, ['Completed ' + str(jobNumber) + ' jobs. Making checkpoint at %3d%%' % (np.round(jobNumber / float(len(jobsToRun)) * 100))])
                 savePickle(jobsToRun, savePickleName)
     except Exception as errorMessage: