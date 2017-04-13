06680c6efc7150f002160f56c4aa3b1cb7f8fd0b
Matty Jones <mattyjones@boisestate.edu>
2017-04-13 11:50:08 -0600

    Updated the KMC combiner to python3

--- analysis/combineKMC/combine.py
+++ analysis/combineKMC/combine.py
@@ -1,4 +1,4 @@
-import cPickle as pickle
+import pickle
 import os
 import subprocess as sp
 import sys
@@ -8,20 +8,20 @@ from scipy.sparse import lil_matrix
 
 if __name__ == "__main__":
     outputDir = sys.argv[1]
-    print "Combining outputs..."
+    print("Combining outputs...")
     combinedData = {}
     pickleFiles = []
     for fileName in os.listdir(outputDir):
         if ("KMCResults_" in fileName):
             pickleFiles.append(outputDir + '/' + fileName)
     pickleFiles = sorted(pickleFiles)
-    print "%d pickle files found to combine!" % (len(pickleFiles))
+    print("%d pickle files found to combine!" % (len(pickleFiles)))
     for fileName in pickleFiles:
         # The pickle was repeatedly dumped to, in order to save time.
         # Each dump stream is self-contained, so iteratively unpickle to add the new data.
-        with open(fileName, 'r') as pickleFile:
+        with open(fileName, 'rb') as pickleFile:
             pickledData = pickle.load(pickleFile)
-            for key, val in pickledData.iteritems():
+            for key, val in pickledData.items():
                 if val is None:
                     continue
                 if key not in combinedData:
@@ -29,7 +29,7 @@ if __name__ == "__main__":
                 else:
                     combinedData[key] += val
     # Write out the combined data
-    print "Writing out the combined pickle file..."
-    with open(outputDir + '/KMCResults.pickle', 'w+') as pickleFile:
+    print("Writing out the combined pickle file...")
+    with open(outputDir + '/KMCResults.pickle', 'wb+') as pickleFile:
         pickle.dump(combinedData, pickleFile)
-    print "Complete data written to", outputDir + "/KMCResults.pickle."
+    print("Complete data written to", outputDir + "/KMCResults.pickle.")
