08cfa4cb924f20a560702a7d52b9a4dd0bd50c66
Matty Jones <mattyjones@boisestate.edu>
2017-05-17 11:34:15 -0600

    Tidied up the repo ready to move to master

--- analysis/decomposeMorphology/decomposeOutputs.py
+++ analysis/decomposeMorphology/decomposeOutputs.py
@@ -157,7 +157,7 @@ if __name__ == "__main__":
                         element[index] = lookup[value]
                     newData.append(element)
             newMorphDict[key] = newData
-        helperFunctions.writeMorphologyXML(newMorphDict, './trimmedFiles/testMorph.xml')
+        helperFunctions.writeMorphologyXML(newMorphDict, './outputFiles/testMorph.xml')
 
     if generateNewPickleFile == True:
         AAfileName, CGMoleculeDict, UnrelaxedAAMorphologyDict, CGtoAAIDs, moleculeAAIDs, boxSize = loadMorphPickle('./p1-L15-f0.0-P0.1-T2.25-e0.5.pickle')
@@ -166,7 +166,7 @@ if __name__ == "__main__":
         CGMoleculeDict = trimCGMoleculeDict(CGMoleculeDict, CGtoAAIDs)
         UnrelaxedAAMorphologyDict = trimBonds(UnrelaxedAAMorphologyDict, lookup)
         toPickle = (AAfileName, CGMoleculeDict, UnrelaxedAAMorphologyDict, CGtoAAIDs, moleculeAAIDs, boxSize)
-        with open('./trimmedFiles/testMorph.pickle', 'w+') as pickleFile:
+        with open('./outputFiles/testMorph.pickle', 'w+') as pickleFile:
             pickle.dump(toPickle, pickleFile)
 
 
