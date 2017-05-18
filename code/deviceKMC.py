bbcd16c0b091af8b8fffe533bc9cdb6a3ec418af
Matty Jones <mattyjones@boisestate.edu>
2017-05-18 17:10:06 -0600

    Morphology read-in for device simulations is now complete

--- code/deviceKMC.py
+++ code/deviceKMC.py
@@ -1,23 +1,52 @@
 import os
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
+try:
+    import mpl_toolkits.mplot3d as p3
+except ImportError:
+    print("Could not import 3D plotting engine, calling the plotMolecule3D function will result in an error!")
 
 import helperFunctions
 
 
 def loadDeviceMorphology(parameterDict):
     deviceDir = parameterDict['inputDeviceDir'] + '/' + parameterDict['deviceMorphology']
-    zvals = os.listdir(deviceDir)
-    deviceArray = np.zeroes([len(zvals)]*3)
-    print deviceArray
+    ySlices = os.listdir(deviceDir)
+    # Initialize the array of the correct size (assumes cubic morphology)
+    deviceArray = np.zeros([len(ySlices)]*3, dtype=int)
+
+    for yVal, fileName in enumerate(ySlices):
+        # Load the ySlice as-presented in the input files
+        ySlice = np.loadtxt(deviceDir + '/' + fileName, dtype=int)
+        # The z-origin is at the top, and we need it at the bottom, so turn the array upside down
+        ySlice = np.flipud(ySlice)
+        # Now populate the array
+        for zVal, zRow in enumerate(ySlice):
+            for xVal, datum in enumerate(zRow):
+                deviceArray[xVal, yVal, zVal] = datum
+
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+    colour = ['r', 'g', 'b', 'y', 'k']
+    for xVal in range(9):
+        for yVal in range(9):
+            for zVal in range(9):
+                ax.scatter(xVal, yVal, zVal, zdir='z', c= colour[deviceArray[xVal, yVal, zVal]])
+    plt.show()
     exit()
-    for zval in os.listdir(deviceDir):
-        pass
 
 
-def execute(chromophoreList, parameterDict):
+def execute(parameterDict):
     # First job will be to load in the device morphology, when I work out what format I want it to be.
+    loadDeviceMorphology(parameterDict)
     return
+
+def coldRun(**kwargs):
+    parameterDict = {}
+    for key, value in kwargs.items():
+        parameterDict[key] = value
+    execute(parameterDict)
 
 
 if __name__ == "__main__":
