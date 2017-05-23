8396addfb2f43cc16b5d1f41b1186810a7ef5b0c
Matty Jones <mattyjones@boisestate.edu>
2017-05-23 09:19:18 -0600

    Started the device simulation code, but want to try a high-resolution version first so switching to a new branch

--- code/deviceKMC.py
+++ code/deviceKMC.py
@@ -5,9 +5,71 @@ import matplotlib.pyplot as plt
 try:
     import mpl_toolkits.mplot3d as p3
 except ImportError:
-    print("Could not import 3D plotting engine, calling the plotMolecule3D function will result in an error!")
+    print("Could not import 3D plotting engine, calling the plotDeviceComponents function will result in an error!")
 
 import helperFunctions
+
+
+class morphologyMoiety:
+    def __init__(self, moietyTypeNumber, molMorphName, parameterDict):
+        self.typeNo = moietyTypeNumber
+        chromophoreListLocation = parameterDict['outputMorphDir'] + '/' + molMorphName + '/code/' + molMorphName + '.pickle'
+        AAMorphologyDict, CGMorphologyDict, CGToAAIDMaster, self.parameterDict, self.chromophoreList = helperFunctions.loadPickle(chromophoreListLocation)
+        self.carrierType = self.getCarrierType()
+        self.TIDist, self.deltaEDist, self.avHopDistance = self.getDistributions(AAMorphologyDict)
+
+    def getCarrierType(self):
+        speciesPresent = []
+        for chromophore in self.chromophoreList:
+            speciesPresent.append(chromophore.species)
+        if len(set(speciesPresent)) == 1:
+            if speciesPresent[0] == 'Donor':
+                return 'hole'
+            elif speciesPresent[0] == 'Acceptor':
+                return 'electron'
+            else:
+                print("Error in chromophore:")
+                for key, val in chromophore.__dict__.items():
+                    print(key, "=", val)
+                raise SystemError("Chromophore species is neither Donor nor Acceptor")
+        else:
+            return 'both'
+
+    def getDistributions(self, AAMorphologyDict):
+        boxSize = [AAMorphologyDict['lx'], AAMorphologyDict['ly'], AAMorphologyDict['lz']]
+        TIDist = []
+        deltaEDist = []
+        hopDistanceDist = []
+        zValDist = []
+        # Iterate over all chromophores in the molecular morphology
+        for chromophore in self.chromophoreList:
+            for neighbourIndex, neighbourData in enumerate(chromophore.neighbours):
+                neighbourID = neighbourData[0]
+                neighbourImage = neighbourData[1]
+                # Only consider the `forward' hops (to prevent duplication)
+                if neighbourID > chromophore.ID:
+                    # Ignore hops with zero transfer integral
+                    if (chromophore.neighboursTI[neighbourIndex] is not None) and (chromophore.neighboursTI[neighbourIndex] > 0):
+                        # Create a transfer integral distribution that we can select from to approximate how carriers move in this moiety
+                        TIDist.append(chromophore.neighboursTI[neighbourIndex])
+                        # Create a deltaE distribution that we can select from to approximate how carriers move in this moiety
+                        deltaEDist.append(chromophore.neighboursDeltaE[neighbourIndex])
+                        currentChromoPosn = chromophore.posn
+                        neighbourChromoPosn = [self.chromophoreList[neighbourID].posn[axis] + (neighbourImage[axis] * boxSize[axis]) for axis in range(3)]
+                        # Create a hopping distance distribution that we can select from to approximate how carriers move in this moiety
+                        hopDistanceDist.append(np.linalg.norm(np.array(neighbourChromoPosn) - np.array(currentChromoPosn)))
+                        # 
+        plotHopDistance(hopDistanceDist)
+        return TIDist, deltaEDist, np.average(hopDistanceDist)
+
+
+def plotHopDistance(distribution):
+    plt.figure()
+    plt.hist(distribution)
+    plt.show()
+    exit()
+
+
 
 
 def loadDeviceMorphology(parameterDict):
@@ -15,7 +77,6 @@ def loadDeviceMorphology(parameterDict):
     ySlices = os.listdir(deviceDir)
     # Initialize the array of the correct size (assumes cubic morphology)
     deviceArray = np.zeros([len(ySlices)]*3, dtype=int)
-
     for yVal, fileName in enumerate(ySlices):
         # Load the ySlice as-presented in the input files
         ySlice = np.loadtxt(deviceDir + '/' + fileName, dtype=int)
@@ -25,7 +86,15 @@ def loadDeviceMorphology(parameterDict):
         for zVal, zRow in enumerate(ySlice):
             for xVal, datum in enumerate(zRow):
                 deviceArray[xVal, yVal, zVal] = datum
+    moietyDictionary = {}
+    for moietyID in np.unique(deviceArray):
+        moietyDictionary[moietyID] = morphologyMoiety(moietyID, parameterDict['deviceComponents'][moietyID], parameterDict)
 
+
+    #plotDeviceComponents(deviceArray)
+
+
+def plotDeviceComponents(deviceArray):
     fig = plt.figure()
     ax = p3.Axes3D(fig)
     colour = ['r', 'g', 'b', 'y', 'k']
@@ -42,7 +111,10 @@ def execute(parameterDict):
     loadDeviceMorphology(parameterDict)
     return
 
+
+# DEBUG FUNCTION, PLEASE IGNORE
 def coldRun(**kwargs):
+# DEBUG FUNCTION, PLEASE IGNORE
     parameterDict = {}
     for key, value in kwargs.items():
         parameterDict[key] = value
