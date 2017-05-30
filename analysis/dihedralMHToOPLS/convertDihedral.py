09debcc6316a6e051621c59b39ef09402dea8bc2
Matty Jones <mattyjones@boisestate.edu>
2017-05-30 09:39:04 -0600

    Tried to update the dihedral conversion script to permit changing from OPLS to MH. The algebra does not work.

--- analysis/dihedralMHToOPLS/convertDihedral.py
+++ analysis/dihedralMHToOPLS/convertDihedral.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def multiHarmonicTorsion(theta, V0, V1, V2, V3, V4):
+def MH(theta, V0, V1, V2, V3, V4):
     # Definition of multiharmonic dihedral equation based on 5 input parameters to be used by HOOMD
     V = V0 + (V1 * np.cos(theta)) + (V2 * ((np.cos(theta))**2)) + (V3 * ((np.cos(theta))**3)) + (V4 * ((np.cos(theta))**4))
     return V
@@ -14,7 +14,7 @@ def OPLS(theta, k1, k2, k3, k4):
     return V
 
 
-def myAlgebra(k1, k2, k3, k4):
+def OPLStoMH(k1, k2, k3, k4):
     V0 = 0.5 * (k1 + (2 * k2) + k3)
     V1 = 0.5 * (k1 - (3 * k3))
     V2 = (4 * k4) - k2
@@ -23,8 +23,16 @@ def myAlgebra(k1, k2, k3, k4):
     return V0, V1, V2, V3, V4
 
 
+def MHtoOPLS(V0, V1, V2, V3, V4):
+    k1 = (2 * V1) + (1.5 * V3)
+    k2 = -V4 - V2
+    k3 = 0.5 * V3
+    k4 = -0.25 * V4
+    return k1, k2, k3, k4
+
+
 if __name__ == "__main__":
-    plotting = False
+    plotting = True
     theta = np.arange(0, 2*np.pi, np.pi/32.)
     k1Vals = [0.0, 0.0, 0.0, 0.0, 0.0, 1.3, 0.0, 0.0, 4.669, 0.0, 0.0, 1.3, 0.0, 0.0, 0.0]
     k2Vals = [14.5, 14.5, 14.5, 14.5, 14.5, -0.05, 0.0, 0.0, 5.124, 5.124, 0.0, -0.05, 0.0, 14.5, 14.5]
@@ -42,7 +50,7 @@ if __name__ == "__main__":
         k3 = k3Vals[index]
         k4 = k4Vals[index]
         actualPot = OPLS(theta, k1, k2, k3, k4)
-        V0, V1, V2, V3, V4 = myAlgebra(k1, k2, k3, k4)
+        V0, V1, V2, V3, V4 = OPLStoMH(k1, k2, k3, k4)
         V0Vals.append(V0)
         V1Vals.append(V1)
         V2Vals.append(V2)
@@ -50,24 +58,43 @@ if __name__ == "__main__":
         V4Vals.append(V4)
 
         if plotting is True:
-            calcPot = multiHarmonicTorsion(theta, V0, V1, V2, V3, V4)
+            calcPot = MH(theta, V0, V1, V2, V3, V4)
 
             plt.figure()
-            plt.plot(theta, actualPot, 'r')
-            plt.savefig('./actualPot.pdf')
-
-            plt.clf()
-            plt.plot(theta, calcPot, 'b')
-            plt.savefig('./calcPot.pdf')
-
-            plt.clf()
             plt.plot(theta, actualPot, 'r', linewidth = 4.0)
             plt.plot(theta, calcPot, 'b', linewidth = 2.0)
-            plt.savefig('./combined.pdf')
+            plt.savefig('./combined_forward_' + str(index) + '.pdf')
 
-    for index, k1 in enumerate(k1Vals):
-        print "\n"
-        print "The dihedral with OPLS Coeffs: %.4f, %.4f, %.4f, %.4f" % (k1, k2Vals[index], k3Vals[index], k4Vals[index])
-        print "Should be replaced with these MultiHarmonic Coeffs: %.4f, %.4f, %.4f, %.4f, %.4f" % (V0Vals[index], V1Vals[index], V2Vals[index], V3Vals[index], V4Vals[index])
-        raw_input("PAUSE...")
+    k1Vals = []
+    k2Vals = []
+    k3Vals = []
+    k4Vals = []
+
+    for index, V0 in enumerate(V0Vals):
+        V1 = V1Vals[index]
+        V2 = V1Vals[index]
+        V3 = V1Vals[index]
+        V4 = V1Vals[index]
+        actualPot = MH(theta, V0, V1, V2, V3, V4)
+        k1, k2, k3, k4 = MHtoOPLS(V0, V1, V2, V3, V4)
+        k1Vals.append(k1)
+        k2Vals.append(k2)
+        k3Vals.append(k3)
+        k4Vals.append(k4)
+
+        if plotting is True:
+            calcPot = OPLS(theta, k1, k2, k3, k4)
+
+            plt.figure()
+            plt.plot(theta, actualPot, 'r', linewidth = 4.0)
+            plt.plot(theta, calcPot, 'b', linewidth = 2.0)
+            plt.savefig('./combined_backward_' + str(index) + '.pdf')
+
+
+
+    #for index, k1 in enumerate(k1Vals):
+    #    print "\n"
+    #    print "The dihedral with OPLS Coeffs: %.4f, %.4f, %.4f, %.4f" % (k1, k2Vals[index], k3Vals[index], k4Vals[index])
+    #    print "Should be replaced with these MultiHarmonic Coeffs: %.4f, %.4f, %.4f, %.4f, %.4f" % (V0Vals[index], V1Vals[index], V2Vals[index], V3Vals[index], V4Vals[index])
+    #    raw_input("PAUSE...")
 
