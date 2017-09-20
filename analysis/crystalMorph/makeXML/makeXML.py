41e04c97e6693ac3b850bbad1bbf54e8afae379e
Matty Jones <mattyjones@boisestate.edu>
2017-09-20 14:32:13 -0600

    Modified the crystal morphology generation analysis script to make a perfect bilayer

--- analysis/crystalMorph/makeXML/makeXML.py
+++ analysis/crystalMorph/makeXML/makeXML.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-sys.path.append('../../code')
+sys.path.append('../../../code')
 import helperFunctions
 
 if __name__ == "__main__":
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     morphDict['lx'] = 100.0
     morphDict['ly'] = 100.0
     morphDict['lz'] = 100.0
-    lastVal = 'A'
     for xVal in np.arange(-45.0, 50.0, 10.0):
         for yVal in np.arange(-45.0, 50.0, 10.0):
             for zVal in np.arange(-45.0, 50.0, 10.0):
@@ -17,13 +16,11 @@ if __name__ == "__main__":
                 morphDict['mass'].append(1.0)
                 morphDict['image'].append([0, 0, 0])
                 morphDict['body'].append(len(morphDict['position']) - 1)
-                if lastVal == 'A':
+                if zVal > 0:
                     morphDict['type'].append('D')
-                    lastVal = 'D'
                 else:
                     morphDict['type'].append('A')
-                    lastVal = 'A'
                 morphDict['diameter'].append(1.0)
                 morphDict['charge'].append(0.0)
     morphDict['natoms'] = len(morphDict['position'])
-    helperFunctions.writeMorphologyXML(morphDict, './mixedCrystal.xml')
+    helperFunctions.writeMorphologyXML(morphDict, './mixedCrystalBilayer.xml')
