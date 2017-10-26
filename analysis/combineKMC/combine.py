d499348de72881d06282efd1f90bb509716bdd73
Evan_Miller <evanmiller326@boisestate.edu>
2017-10-26 14:04:37 -0600

    Updating combine.py with the same as hiRes

--- analysis/combineKMC/combine.py
+++ analysis/combineKMC/combine.py
@@ -3,10 +3,30 @@ import os
 import subprocess as sp
 import sys
 from scipy.sparse import lil_matrix
+import glob
+import re
+from shutil import copyfile
 
-
+def create_results():
+    cores_list = []
+    for core in glob.glob("KMCData_*.pickle"):
+        cores_list.append(re.findall("KMCData_(.*).pickle", core)[0])
+    keep_list = []
+    for core in cores_list:
+        select_list = []
+        size1 = 'KMCslot1Results_'+str(core)+".pickle"
+        size2 = 'KMCslot2Results_'+str(core)+".pickle"
+        if size1 >= size2:
+            keep_list.append(size1)
+        else:
+            keep_list.append(size2)
+    print(keep_list)
+    for keeper in zip(cores_list, keep_list):
+        new_name = "KMCResults_"+str(keeper[0])+".pickle"
+        copyfile(str(keeper[1]), new_name)
 
 if __name__ == "__main__":
+    create_results()
     outputDir = sys.argv[1]
     print("Combining outputs...")
     combinedData = {}
@@ -22,12 +42,15 @@ if __name__ == "__main__":
         with open(fileName, 'rb') as pickleFile:
             pickledData = pickle.load(pickleFile)
             for key, val in pickledData.items():
-                if val is None:
-                    continue
-                if key not in combinedData:
-                    combinedData[key] = val
-                else:
-                    combinedData[key] += val
+                try:
+                    if val is None:
+                        continue
+                    if key not in combinedData:
+                        combinedData[key] = val
+                    else:
+                        combinedData[key] += val
+                except AttributeError:
+                    pass
     # Write out the combined data
     print("Writing out the combined pickle file...")
     with open(outputDir + '/KMCResults.pickle', 'wb+') as pickleFile:
