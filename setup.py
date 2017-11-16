b8635c0257d5003d74b2ccc276779303adb71da4
Mike Henry <mikehenry@boisestate.edu>
2017-11-16 10:41:37 -0700

    added convertToXYZ to a entry point, used inp2xyz to convert, expects a list of files or a file, puts ouput in same dir as the file is

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'sample=sample:main',
+            'inp2xyz=morphct.utils.inpToXYZ.convertToXYZ:convert',
         ],
     },
 )
