7da4c2d75bf876d56e12b3c13b6e97c3f8f7dc41
Matty Jones <mattyjones@boisestate.edu>
2018-08-03 14:48:06 -0600

    Added some additional functionality to the error bars script

--- morphct/utils/add_error_bars.py
+++ morphct/utils/add_error_bars.py
@@ -1,4 +1,5 @@
 from morphct.code import helper_functions as hf
+from collections import OrderedDict
 import numpy as np
 import glob
 import argparse
@@ -16,7 +17,7 @@ def split_argument_into_dictionary(argument):
         combine_list - dictionary
     """
     #Create empty dictionary
-    combine_list = {}
+    combine_list = OrderedDict()
     #Remove curly brackets
     argument = argument[1:-1]
     #Remove whitespace
@@ -37,8 +38,8 @@ def split_argument_into_dictionary(argument):
         sublist = []
         #Split the list based off commas
         for subitems in items.split(','):
-            #Run a glob on the items in the list
-            runs = glob.glob(subitems)
+            #Run a glob on the items in the list (remove quotes from around string)
+            runs = glob.glob(subitems[1:-1])
             #Add the items in the glob to the sublist
             for run in runs:
                 sublist.append(run)
@@ -110,14 +111,17 @@ def plot_data(data, title, xlabel = "Order-Semi-Disorder"):
 
     x_data, y_data, y_error = zip(*sorted(zip(data[:,0], data[:,1], data[:,2])))
     #Plot the errors
-    plt.errorbar(x_data, y_data, yerr=y_error)
+    plt.errorbar(x_data, y_data, yerr=y_error, c='r')
     plt.yscale('log')
+    #plt.yticks([2E-2, 3E-2, 4E-2, 5E-2, 6E-2, 7E-2, 8E-2, 9E-2, 1E-1], [r'$2\times10^{-2}$','',r'$4\times10^{-2}$','','','','',
+    #    '',r'$1\times10^{-1}$'])
     plt.ylabel(r"Mobility (cm$^{2}$V$^{-1}$s$^{-1}$)")
+    #plt.ylabel(r"Anisotropy (Arb. U.)")
     plt.xlabel(xlabel)
     #plt.title(title)
     plt.savefig("output/{}.pdf".format(title))
 
-def save_mean_data_to_csv(data):
+def save_mean_data_to_csv(data, prop):
     """
     Write the average and standard error data
     to a file.
@@ -134,13 +138,13 @@ def save_mean_data_to_csv(data):
         text +="{},{},{}\n".format(line[0], line[1], line[2])
 
     #Will over write data.csv each time.
-    with open("output/data.csv", 'w') as f:
+    with open(os.path.join("output", "".join([prop, ".csv"])), 'w+') as f:
         f.writelines(text)
 
-def calc_mean_and_dev(combine_list, x_label, properties = ['hole_mobility']):
+def calc_mean_and_dev(combine_list, sequence, x_label, prop = 'hole_mobility'):
     """
     Iterate through the dictionary of lists to get the
-    information from the appropriate csv files, then calculate 
+    information from the appropriate csv files, then calculate
     the average and standard error of the data.
     Plot and write this data to files.
     Requires:
@@ -166,41 +170,49 @@ def calc_mean_and_dev(combine_list, x_label, properties = ['hole_mobility']):
     #Turn data into lists and arrays for writing and plotting
     #the desired properties.
     data_list = []
-    #Go through the properties first for plotting each property separately.
-    for prop in properties:
-        property_list = []
-        #Get the average and deviation from each run for the desired property.
-        for key, pair in total_data.items():
-            name = "{}_{}".format(key, prop)
-            p_data = total_data[key][prop]
-            p_mean = np.mean(p_data)
-            p_error = np.std(p_data)/np.sqrt(len(p_data))
-            data_list.append([name, p_mean, p_error])
-            #If the keys are numbers write to list
-            try:
-                property_list.append([float(key), p_mean, p_error])
-            #Else convert to number based on its index in the list
-            except:
-                property_list.append([keys_list.index(key), p_mean, p_error])
-        #Plot the data
-        plot_data(np.array(property_list), prop, xlabel=x_label)
+    property_list = []
+    #Get the average and deviation from each run for the desired property.
+    for key, pair in total_data.items():
+        name = "{}_{}".format(key, prop)
+        p_data = total_data[key][prop]
+        p_mean = np.mean(p_data)
+        p_error = np.std(p_data)/np.sqrt(len(p_data))
+        data_list.append([name, p_mean, p_error])
+        #If the keys are numbers write to list
+        try:
+            property_list.append([float(key), p_mean, p_error])
+        #Else convert to number based on its index in the list
+        except:
+            property_list.append([sequence[keys_list.index(key)], p_mean, p_error])
+    #Plot the data
+    plot_data(np.array(property_list), prop, xlabel=x_label)
     #Write all the data into a single file
-    save_mean_data_to_csv(data_list)
+    save_mean_data_to_csv(data_list, prop)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--combine", default=None, required=True,
+    parser.add_argument("-c", "--combine", required=True,
                         type = split_argument_into_dictionary,
-                        help=('''Combine mobility calculations from list of runs to create error bars.'
-                            'Takes a dictionary where keys are strings and pairs are lists of directories.'
-                            'Glob is applied to items within lists so that wildcards will 
-                            expand all items fitting the glob into the list.
-                            'For example: "{'crystal':[cryst-*, large_p3ht_ordered],}"
-                            will produce: {'crystal':['cryst-frame-1', 'cryst-frame-2',
-                            ...'cryst-frame-N', 'large_p3ht_ordered']
-                            'Function will look for result.csv files for mobility if they exist.'
-                            '''))
-    parser.add_argument("-x", "--x_label", default=None, required=True,
-                        help=('''Set an x label for the final plot'''))
+                        help=("""Combine mobility calculations from list of runs to create error bars.
+                              Takes a dictionary where keys are strings and pairs are lists of directories.
+                              Glob is applied to items within lists so that wildcards will
+                              expand all items fitting the glob into the list.
+                              For example: "{'crystal':[cryst-*, large_p3ht_ordered],}
+                              will produce: {'crystal':['cryst-frame-1', 'cryst-frame-2',
+                              ...'cryst-frame-N', 'large_p3ht_ordered']
+                              Function will look for result.csv files for mobility if they exist.
+                              """))
+    parser.add_argument("-s", "--sequence", required=True,
+                        type=lambda s: [float(item) for item in s.split(',')], default=None,
+                        help=("""Create a figure in the current directory that describes the evolution
+                              of the anisotropy/mobility using the specified comma-delimited string
+                              as the sequence of x values. For instance -s '1.5,1.75,2.0,2.25,2.5'
+                              will assign each of the 5 keys in the args.combine list to these x-values when
+                              plotting the errorbar graph."""))
+    parser.add_argument("-x", "--x_label", required=True,
+                        help=("""Set an x label for the final plot"""))
+    parser.add_argument("-p", "--prop", required=True,
+                        help=("""The property, as specified in the results.csv, to use during the aggregation
+                              of the data and the error bar calculation."""))
     args, directory_list = parser.parse_known_args()
-    calc_mean_and_dev(args.combine, args.x_label)
+    calc_mean_and_dev(args.combine, args.sequence, args.x_label, prop=args.prop)
