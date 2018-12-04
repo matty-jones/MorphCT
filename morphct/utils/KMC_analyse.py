2f1c0d346921af2df2ad8416b7fd460df1c3ee1c
Matthew Jones <mattyjones@boisestate.edu>
2018-12-04 15:09:08 -0700

    Finished the plot_hop_vectors function in KMCA

--- morphct/utils/KMC_analyse.py
+++ morphct/utils/KMC_analyse.py
@@ -286,11 +286,11 @@ def plot_connections(
     colour_map = plt.get_cmap("inferno")
     c_norm = matplotlib.colors.Normalize(vmin=np.floor(vmin), vmax=np.ceil(vmax))
     scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=colour_map)
-    hopcolors = scalar_map.to_rgba(connections_array[:, 6])
+    hop_colors = scalar_map.to_rgba(connections_array[:, 6])
 
     # Set up the intensity for the hops so more travelled paths are more intense
     alphas = connections_array[:, 6] / vmax
-    hopcolors[:, 3] = alphas
+    hop_colors[:, 3] = alphas
 
     # Plot the vectors between two chromophores
     ax.quiver(
@@ -300,7 +300,7 @@ def plot_connections(
         connections_array[:, 3],
         connections_array[:, 4],
         connections_array[:, 5],
-        color=hopcolors,
+        color=hop_colors,
         arrow_length_ratio=0,
         linewidth=0.7,
     )
@@ -580,9 +580,17 @@ def plot_hop_vectors(
     carrier_data, AA_morphology_dict, chromophore_list, directory, sim_dims,
     carrier_type, plot3D_graphs
 ):
+    import matplotlib.colors
+    import matplotlib.cm as cmx
+
+    if not plot3d_graphs:
+        return
+
     box_lengths = np.array([axis[1] - axis[0] for axis in sim_dims])
     carrier_history = carrier_data["carrier_history_matrix"]
     non_zero_coords = list(zip(*carrier_history.nonzero()))
+    hop_vectors = []
+    intensities = []
     for (chromo1, chromo2) in non_zero_coords:
         # Find the normal of chromo1 plane (first three atoms)
         triplet_posn = [
@@ -607,150 +615,57 @@ def plot_hop_vectors(
         # Apply rotation matrix to chromo1-chromo2 vector
         hop_vector = np.matmul(rotation_matrix, unrotated_hop_vector)
         # Store the hop vector and the intensity
-        intensity = carrier_history[chromo1, chromo2]
+        intensity = np.log10(carrier_history[chromo1, chromo2])
+        hop_vectors.append(hop_vector)
+        intensities.append(intensity)
 
-
-    # Use quiver to make the plots
-
-
-
-    # A complicated function that shows connections between carriers in 3D that carriers prefer to hop between.
-    # Connections that are frequently used are highlighted in black, whereas rarely used connections are more white.
-    # Import matplotlib color modules to set up color bar.
-    import matplotlib.colors
-    import matplotlib.cm as cmx
-
+    # Convert hop_vectors to an np.array
+    hop_vectors = np.array(hop_vectors)
     # Create a figure class
     fig = plt.figure(figsize=(7, 6))
     # Make a 3D subplot
     ax = fig.add_subplot(111, projection="3d")
-
-    # Create the array for all the chromophore connections
-    connections_array = create_array_for_plot_connections(
-        chromophore_list, carrier_history, sim_dims
-    )
-
-    # Determine the smalles, non-zero number of times two chromophores are connected.
-    vmin = np.min(np.array(connections_array)[:, 6])
+    # Determine the smallest, non-zero number of times two chromophores are connected.
+    I_min = np.min(intensities)
     # Determine the max number of times two chormophores are connected.
-    vmax = np.max(np.array(connections_array)[:, 6])
+    I_max = np.max(intensities)
 
     # Set up the color bar.
-    colour_map = plt.get_cmap("inferno")
-    c_norm = matplotlib.colors.Normalize(vmin=np.floor(vmin), vmax=np.ceil(vmax))
+    colour_map = plt.get_cmap("Greys")
+    c_norm = matplotlib.colors.Normalize(vmin=np.floor(I_min), vmax=np.ceil(I_max))
     scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=colour_map)
-    hopcolors = scalar_map.to_rgba(connections_array[:, 6])
+    hop_colors = scalar_map.to_rgba(intensities)
 
-    # Set up the intensity for the hops so more travelled paths are more intense
-    alphas = connections_array[:, 6] / vmax
-    hopcolors[:, 3] = alphas
+    # # Set up the intensity for the hops so more travelled paths are more intense
+    # alphas = intensities / I_max
+    # hop_colors[:, 3] = alphas
 
     # Plot the vectors between two chromophores
     ax.quiver(
-        connections_array[:, 0],
-        connections_array[:, 1],
-        connections_array[:, 2],
-        connections_array[:, 3],
-        connections_array[:, 4],
-        connections_array[:, 5],
-        color=hopcolors,
+        # X-vals for start of each arrow
+        np.array([0] * len(hop_colors)),
+        # Y-vals for start of each arrow
+        np.array([0] * len(hop_colors)),
+        # Z-vals for start of each arrow
+        np.array([0] * len(hop_colors)),
+        # X-vecs for direction of each arrow
+        hop_vectors[:,0],
+        # Y-vecs for direction of each arrow
+        hop_vectors[:,1],
+        # Z-vecs for direction of each arrow
+        hop_vectors[:,2],
+        color=hop_colors,
         arrow_length_ratio=0,
         linewidth=0.7,
     )
-
-    # Draw boxlines
-    # Varying X
-    ax.plot(
-        [sim_dims[0][0], sim_dims[0][1]],
-        [sim_dims[1][0], sim_dims[1][0]],
-        [sim_dims[2][0], sim_dims[2][0]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][0], sim_dims[0][1]],
-        [sim_dims[1][1], sim_dims[1][1]],
-        [sim_dims[2][0], sim_dims[2][0]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][0], sim_dims[0][1]],
-        [sim_dims[1][0], sim_dims[1][0]],
-        [sim_dims[2][1], sim_dims[2][1]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][0], sim_dims[0][1]],
-        [sim_dims[1][1], sim_dims[1][1]],
-        [sim_dims[2][1], sim_dims[2][1]],
-        c="k",
-        linewidth=1.0,
-    )
-    # Varying Y
-    ax.plot(
-        [sim_dims[0][0], sim_dims[0][0]],
-        [sim_dims[1][0], sim_dims[1][1]],
-        [sim_dims[2][0], sim_dims[2][0]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][1], sim_dims[0][1]],
-        [sim_dims[1][0], sim_dims[1][1]],
-        [sim_dims[2][0], sim_dims[2][0]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][0], sim_dims[0][0]],
-        [sim_dims[1][0], sim_dims[1][1]],
-        [sim_dims[2][1], sim_dims[2][1]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][1], sim_dims[0][1]],
-        [sim_dims[1][0], sim_dims[1][1]],
-        [sim_dims[2][1], sim_dims[2][1]],
-        c="k",
-        linewidth=1.0,
-    )
-    # Varying Z
-    ax.plot(
-        [sim_dims[0][0], sim_dims[0][0]],
-        [sim_dims[1][0], sim_dims[1][0]],
-        [sim_dims[2][0], sim_dims[2][1]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][0], sim_dims[0][0]],
-        [sim_dims[1][1], sim_dims[1][1]],
-        [sim_dims[2][0], sim_dims[2][1]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][1], sim_dims[0][1]],
-        [sim_dims[1][0], sim_dims[1][0]],
-        [sim_dims[2][0], sim_dims[2][1]],
-        c="k",
-        linewidth=1.0,
-    )
-    ax.plot(
-        [sim_dims[0][1], sim_dims[0][1]],
-        [sim_dims[1][1], sim_dims[1][1]],
-        [sim_dims[2][0], sim_dims[2][1]],
-        c="k",
-        linewidth=1.0,
-    )
+    ax.set_xlim([-np.max(hop_vectors[:,0]), np.max(hop_vectors[:,0])])
+    ax.set_ylim([-np.max(hop_vectors[:,1]), np.max(hop_vectors[:,1])])
+    ax.set_zlim([-np.max(hop_vectors[:,2]), np.max(hop_vectors[:,2])])
 
     # Make the colour bar
-    scalar_map.set_array(connections_array[:, 6])
+    scalar_map.set_array(intensities)
     # tick_location = np.arange(0, int(np.ceil(vmax)) + 1, 1)
-    tick_location = np.arange(0, vmax, 1)
+    tick_location = np.arange(0, I_max, 1)
     # Plot the colour bar
     cbar = plt.colorbar(
         scalar_map, ticks=tick_location, shrink=0.8, aspect=20
@@ -769,13 +684,14 @@ def plot_hop_vectors(
         ]
     )
     # plt.title(figure_title, y=1.1)
-    # 01 for donor 3d network, 02 for acceptor 3d network
-    file_name = "".join(["{:02}_3d_".format(1 + carrier_index), carrier_type, ".png"])
+    # 36 for donor 3d network, 37 for acceptor 3d network
+    file_name = "".join(["{:02}_hop_vec_".format(
+        36 + carrier_index), carrier_type, ".png"]
+    )
     plt.savefig(
         os.path.join(directory, "figures", file_name), bbox_inches="tight", dpi=300
     )
     print("Figure saved as", os.path.join(directory, "figures", file_name))
-
     plt.clf()
 
 
