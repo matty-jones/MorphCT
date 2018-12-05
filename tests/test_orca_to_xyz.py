fc5697a36619202a729c2edfe5daed8f0e72581f
Matthew Jones <mattyjones@boisestate.edu>
2018-12-04 17:39:29 -0700

    Added a simple test for orca2xyz

---
+++ tests/test_orca_to_xyz.py
@@ -0,0 +1,56 @@
+import csv
+import os
+import pytest
+import shutil
+import subprocess
+from morphct import run_MorphCT
+from morphct.definitions import TEST_ROOT
+from testing_tools import TestCommand
+from morphct.code import helper_functions as hf
+
+
+@pytest.fixture(scope="module")
+def run_simulation(request):
+    # ---==============================================---
+    # ---=============== Setup Prereqs ================---
+    # ---==============================================---
+
+    output_dir = os.path.join(TEST_ROOT, "output_O2X")
+
+    try:
+        shutil.rmtree(output_dir)
+    except OSError:
+        pass
+    os.makedirs(output_dir)
+    shutil.copy(
+        os.path.join(TEST_ROOT, "assets", "donor_polymer", "EZ", "input_orca", "single", "00002.inp"), os.path.join(output_dir, "00002.inp")
+    )
+
+
+    command = ["orca2xyz", os.path.join(output_dir, "00002.inp")]
+    subprocess.Popen(command).communicate()
+
+
+# ---==============================================---
+# ---================= Run Tests ==================---
+# ---==============================================---
+
+
+class TestCompareOutputs(TestCommand):
+    def test_check_output_exists(self, run_simulation):
+        self.confirm_file_exists(
+            os.path.join(TEST_ROOT, "output_O2X", "00002.xyz")
+        )
+
+
+def teardown_module():
+    shutil.rmtree(os.path.join(TEST_ROOT, "output_O2X"))
+
+
+if __name__ == "__main__":
+
+    class parameters:
+        def __init__(self, param):
+            self.param = param
+
+    run_simulation(parameters(""))
